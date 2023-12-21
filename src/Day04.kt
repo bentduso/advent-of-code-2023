a809f17038a39cdd1db7d496d184fd4b71a47fed
Benjamin Zubača <bzubaca123@gmail.com>
2023-12-21 20:37:35 +0100

    Complete the second part of Day04 puzzles

--- src/Day04.kt
+++ src/Day04.kt
@@ -4,31 +4,47 @@ fun main() {
     with(Day04) {
         val testInput: List<String> = readInput("Day04_test")
         check(solvePart1(testInput) == 13)
+        check(solvePart2(testInput) == 30)
 
         solvePart1(this.input).println()
+        solvePart2(this.input).println()
     }
 }
 
 object Day04 {
     val input: List<String> = readInput("Day04")
 
-    fun solvePart1(input: List<String>): Int = input.map { parseLineToScratchCard(it) }.sumOf { it.points }
+    fun solvePart1(input: List<String>): Int = input.map { it.parseToScratchCard() }.sumOf { it.points }
 
-    private fun parseLineToScratchCard(line: String): ScratchCard {
-        val scratchCardNumbers: String = line.split(": ")[1]
-        val (winningNumbers: Set<Int>, scratchedNumbers: Set<Int>) = scratchCardNumbers.split("|")
-            .map { parseScratchCardNumbersToSet(it) }
+    fun solvePart2(input: List<String>): Int {
+        val cardMatches: List<Int> = input.map { it.parseToScratchCard().matchingNumbers }
+        val scratchCards = IntArray(cardMatches.size) { 1 }
 
-        return ScratchCard(winningNumbers, scratchedNumbers)
+        cardMatches.forEachIndexed { index, matchCount ->
+            repeat(matchCount) { futureCardOffset ->
+                scratchCards.getOrNull(index + futureCardOffset + 1)
+                    ?.let { scratchCards[index + futureCardOffset + 1] += scratchCards[index] }
+            }
+        }
 
+        return scratchCards.sum()
     }
 
-    private fun parseScratchCardNumbersToSet(numbers: String): Set<Int> =
-        numbers.trim().split(Regex("\\s+")).map(String::toInt).toSet()
+    private fun String.parseToScratchCard(): ScratchCard {
+        val scratchCardNumbers: String = this.split(": ")[1]
+        val (winningNumbers: Set<Int>, scratchedNumbers: Set<Int>) = scratchCardNumbers.split("|")
+            .map {
+                it.trim()
+                    .split(Regex("\\s+"))
+                    .map(String::toInt)
+                    .toSet()
+            }
+
+        return ScratchCard(winningNumbers, scratchedNumbers)
+    }
 
     data class ScratchCard(val winningNumbers: Set<Int>, val scratchedNumbers: Set<Int>) {
-        val points: Int
-            get() = winningNumbers.intersect(scratchedNumbers).size
-                .takeIf { it > 0 }?.let { 2.0.pow(it - 1).toInt() } ?: 0
+        val matchingNumbers: Int = winningNumbers.intersect(scratchedNumbers).size
+        val points: Int = matchingNumbers.takeIf { it > 0 }?.let { 2.0.pow(it - 1).toInt() } ?: 0
     }
 }
